8c8ac1cccf6199e3a39c554b162c81249801d2ab
Ramesh Lingappan <ramesh.lingappan@payconiq.com>
2020-07-09 18:19:57 +0200

    changes the setup to be a console app

--- spring-prometheus-gateway/src/main/java/com/example/prometheuspushgateway/Application.java
+++ spring-prometheus-gateway/src/main/java/com/example/prometheuspushgateway/Application.java
@@ -1,12 +1,27 @@
 package com.example.prometheuspushgateway;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class Application {
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner {
+
+    private final HelloService service;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        for (int i = 0; i < 5; i++) {
+            log.info("{} - {}", i, service.getHello());
+        }
     }
 }
--- spring-prometheus-gateway/src/main/java/com/example/prometheuspushgateway/HelloService.java
+++ spring-prometheus-gateway/src/main/java/com/example/prometheuspushgateway/HelloService.java
@@ -1,4 +1,4 @@
-package com.example.prometheuspushgateway.hello;
+package com.example.prometheuspushgateway;
 
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
@@ -14,17 +14,10 @@ public class HelloService {
         this.counter = meterRegistry.counter("hello.counter", "type", "hello");
     }
 
-    /* @Timed annotation avoid boilerplate code like below,
-    *  timer = Timer
-                .builder("hello.process")
-                .description("Time spent processing hello")
-                .tags("type", "hello")
-                .register(meterRegistry);
-    * */
     @Timed(value = "hello-process", description = "Time spent processing hello", extraTags = {"type", "hello"})
     public String getHello() {
         try {
-            Thread.sleep(250);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
